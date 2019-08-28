15e195f49f98ddc533bacac8b0d3ff19de8e7987
Rahardian Juliarto <rahardian.juliarto@traveloka.com>
2019-08-28 17:57:55 +0700

    solved balanced tre problem

--- balancedtree.py
+++ balancedtree.py
@@ -14,16 +14,30 @@ def height(root):
 
 
 def is_balanced(root):
+
+    if root == None :
+        return True
+
+    lh = height(root.left)
+    rh = height(root.right)
+
+    if abs(lh - rh) <= 0 and is_balanced(root.left) and is_balanced(root.right) :
+        return True
+
+    return False
     
 
 root = Node(1)
 root.left = Node(2)
 root.right = Node(3)
+root.right.right = Node(4)
 
-root.left.left = Node(4)
-root.left.right = Node(5)
+# root.left.left = Node(4)
+# root.left.right = Node(5)
 
 
 tree_height = height(root)
 
-print("Height of tree ", tree_height)
+# print("Height of tree ", tree_height)
+
+print("Is balanced : ", is_balanced(root))
